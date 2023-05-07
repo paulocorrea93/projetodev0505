ada9c6504a29867f1de2fe571efa33b0de79e8af
Paulo <paulocorreapc93@gmail.com>
2023-05-06 23:56:39 -0400

    criada função para adicionar numero na tela

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -8,6 +8,15 @@ const Calculator = () => {
     const [oldNum, setOldNum] = useState(0)
     const [operator, setOperator] = useState ()
 
+    function inputNum (e){
+        var input = e.target.value;
+        if(num === 0){
+            setNum(input)
+        } else {
+            setNum(num + input)
+        }
+    }
+
   return (
     <div>
       
