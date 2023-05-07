f4100ba71d3f047265b55a00335b325cf48fe336
Paulo <paulocorreapc93@gmail.com>
2023-05-06 23:57:46 -0400

    criada função para zerar numero na tela

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import './Calculator.css'
-import { useState } from 'react'
+import React from "react";
+import "./Calculator.css";
+import { useState } from "react";
 
 const Calculator = () => {
+  const [num, setNum] = useState(0);
+  const [oldNum, setOldNum] = useState(0);
+  const [operator, setOperator] = useState();
 
-    const [num, setNum] = useState(0)
-    const [oldNum, setOldNum] = useState(0)
-    const [operator, setOperator] = useState ()
-
-    function inputNum (e){
-        var input = e.target.value;
-        if(num === 0){
-            setNum(input)
-        } else {
-            setNum(num + input)
-        }
+  function inputNum(e) {
+    var input = e.target.value;
+    if (num === 0) {
+      setNum(input);
+    } else {
+      setNum(num + input);
     }
+  }
 
   return (
     <div>
-      
+      <p></p>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
