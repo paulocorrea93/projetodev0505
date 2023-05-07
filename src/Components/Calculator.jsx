1c3db63f0342b9847326af8e49c4836a0c20b4d9
Paulo <paulocorreapc93@gmail.com>
2023-05-06 23:54:47 -0400

    adicionados as variaveis que receberão 'useState'

---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Calculator.css'
+import { useState } from 'react'
+
+const Calculator = () => {
+
+    const [num, setNum] = useState(0)
+    const [oldNum, setOldNum] = useState(0)
+    const [operator, setOperator] = useState ()
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Calculator
